25f06f24478e98ff0a3260ad548d3293d8df5217
Antani Kalchev <antatoni@abv.bg>
2025-08-27 13:56:38 +0300

    very very basic OS module demo

---
+++ node.js/osDemo.js
@@ -0,0 +1,14 @@
+import os from 'os';
+
+//userInfo() - gives info about currently logged user in the OS
+console.log(os.userInfo());
+
+//totalMem() - gives total memory in BYTES
+console.log(os.totalmem());
+
+//freemem() - gives total memory in BYTES
+
+console.log(os.freemem());
+
+//cpus() object with all cores !
+console.log(os.cpus());
